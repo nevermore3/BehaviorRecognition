67c85799dd65c8b60dcb4b913bd0ff7bb872f603
libingchen-intsig <libingchen@intsig.net>
2020-04-26 22:32:18 +0800

    fix

--- models.py
+++ models.py
@@ -2,6 +2,7 @@
 import logging
 import libsvm.svm as svm
 import libsvm.svmutil as svmutil
+import commons
 
 class BaseModel(object):
     def __init__(self):
@@ -59,19 +60,24 @@ class LIBSVMModel(BaseModel):
         The k in the -g option means the number of attributes in the input data.
     """
     def train(self, input_data_path, params="-t 0 -c 4 -b 1'"):
-        self._data = svmutil.svm_read_problem(input_data_path + "/Train.txt")
+        with commons.PhaseLogger("LIBSVM.train.read_problem"):
+            self._data = svmutil.svm_read_problem(input_data_path + "/Train.txt")
         self._params = svmutil.svm_parameter(params)
-        self._model, acc, mse = svmutil.svm_train(self._data, self._params)
+        with commons.PhaseLogger("LIBSVM.train.svm_train"):
+            self._model, acc, mse = svmutil.svm_train(self._data, self._params)
         self._init = True
         logging.info("[%s]: train with Acc[%.4f] and Mse[%.4f]" % (self._get_class_name(), acc, mse))
     
     def save(self, save_path):
-        svmutil.save_svm_model(save_path + "/" + self._get_class_name() + ".model", self._model)
+        with commons.PhaseLogger("LIBSVM.save"):
+            svmutil.save_svm_model(save_path + "/" + self._get_class_name() + ".model", self._model)
         logging.info("[%s]: Save Model Done")
     
     def evaluate(self, input_data_path):
-        Y, X = svmutil.svm_read_problem(input_data_path + "/Test.txt")
-        p_labels, p_acc, p_vals = svmutil.svm_predict(Y, X, self._model)
+        with commons.PhaseLogger("LIBSVM.evaluate.read_problem"):
+            Y, X = svmutil.svm_read_problem(input_data_path + "/Test.txt")
+        with commons.PhaseLogger("LIBSVM.evaluate.predict"):
+            p_labels, p_acc, p_vals = svmutil.svm_predict(Y, X, self._model)
         logging.info("[%s]: evaluate with Acc[%.4f]" % (self._get_class_name(), p_acc))
 
 
