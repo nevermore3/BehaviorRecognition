c6ec141b87ae073581fdbd532bb5f7ac0085a008
jmq <58863790@qq.com>
2020-04-27 11:28:47 +0800

    main

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ import models
 import argparse
 import preprocesses
 import commons
-
+import logging
+logging.basicConfig(level=logging.DEBUG)
 def main(args):
     # preprocess original raw data
     with commons.PhaseLogger("Preprocess TrainSet"):
@@ -22,10 +23,10 @@ def main(args):
     with commons.PhaseLogger("Saving Model"):
         # save model
         model.save(args.intermediate_data)
-    with commons.PhaseLogger("Evaluating Model"):
+    #with commons.PhaseLogger("Evaluating Model"):
         # test model
-        preprocessor.handleData(args.test_input, args.intermediate_data, is_train=False)
-        model.evaluate(args.intermediate_data)
+    #    preprocessor.handleData(args.test_input, args.intermediate_data, is_train=False)
+    #    model.evaluate(args.intermediate_data)
 
 
 
@@ -34,8 +35,8 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('CommandLine Parser')
     parser.add_argument('-m', '--model', dest='model', type=str, default="LIBSVM", help="model that was used for train and test, currently only support LIBSVM")
-    parser.add_argument('-i', '--input', dest='input', type=str, default="./data/", help="")
-    parser.add_argument('-t', '--intermediate_data', dest='intermediate_data', type=str, default="./data_int/", help="intermediate_data filepath")
+    parser.add_argument('-i', '--input', dest='train_input', type=str, default="..\\data", help="")
+    parser.add_argument('-t', '--intermediate_data', dest='intermediate_data', type=str, default="..\\data", help="intermediate_data filepath")
     parser.add_argument('-p', '--preprocess', dest='preprocess', type=str, default="LIBSVM", help="preprocessor that were defined inside preprocess.py")
     args = parser.parse_args()
     main(args)
